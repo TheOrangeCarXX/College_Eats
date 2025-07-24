c085154aeaef9696a035064558373433dffa75da
Yashwanth Reddy <yashwanththeboss05@gmail.com>
2025-07-25 01:14:05 +0530

    Versionn 2-modal added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,44 @@
-import { useState } from 'react'
 import './App.css'
 import Modal from "react-modal";
-import logo from "./assets/college_eat.svg";
+import Logo from './Logo.jsx';
+import Button from './Button.jsx';
+import { useState } from 'react';
+import './Modal.css';
 
 Modal.setAppElement('#root');
 function App() {
-  const [signinModalIsOpen, setSigninModalIsOpen] = useState(false);
-  const [loginModalIsOpen, setLoginModalIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  function handleSubmit(event) {
+    event.preventDefault();
+
+  }
   return (
     <>
-    <img src={logo} alt="College Eats Logo" className="college-eats-logo"/>
+      <Logo/>
       <h3>College Eats</h3>
       <p className="slogan">
         Together Tastes Better
       </p>
       <h1>Order Food Together, Save Together</h1>
-      <button className="sign-in-button" onClick={()=>setSigninModalIsOpen(true)}>Get Started</button>
-      <button className="login-button" onClick={()=>setLoginModalIsOpen(true)}>Login</button>
+      <Button onClick= {() => setIsModalOpen(true)}>Sign In</Button>
+      <Button> Login In</Button>
+      <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)} className="modal-content" overlayClassName="modal-overlay">
+        <h2>Sign In</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required placeholder='Enter Email' onChange={e => setEmail(e.target.value)}/>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required placeholder="Enter Password" onChange={e => setPassword(e.target.value)}/>
+          <Button type="submit">Submit</Button>
+        </form>
+        <Button onClick={() => setIsModalOpen(false)}>Close</Button>
+      </Modal>
+      <footer>
+        <p>© 2023 College Eats</p>
+        <p>All rights reserved</p>
+      </footer>
     </>
   )
 }
---
+++ Frontend/src/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from './assets/college_eat.svg';
+import './Logo.css';
+
+function Logo() {
+    return (
+        <img src={logo} alt="College Eats Logo" className="logo" />
+    );
+}
+
+export default Logo;
